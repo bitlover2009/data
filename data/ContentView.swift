2fb3af5d5a52ae51034b3d3b9b2a171f5525e208
bitlover2009 <bitlover2009@gmail.com>
2024-01-13 19:36:25 -0600

    Initial Commit

---
+++ data/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  data
+//
+//  Created by David Grau Beltrán  on 13/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ data/dataApp.swift
@@ -0,0 +1,17 @@
+//
+//  dataApp.swift
+//  data
+//
+//  Created by David Grau Beltrán  on 13/01/24.
+//
+
+import SwiftUI
+
+@main
+struct dataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
